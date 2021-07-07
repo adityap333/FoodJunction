f6ae3db9fbd510f9ac93dc479707cacc2d95db5f
Aditya Pujari <adityapujari333@gmail.com>
2021-07-07 23:30:44 +0530

    Spring Boot Project Initialized
    New Spring boot added with following dependencies Web, DevTools, JPA, H2.

---
+++ src/main/java/com/foodJunction/FoodJunctionApplication.java
@@ -0,0 +1,13 @@
+package com.foodJunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodJunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodJunctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodJunction/FoodJunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodJunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodJunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
